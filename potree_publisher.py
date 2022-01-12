db56d1090f953e2b9143d00fccda90b8b5341615
Elie-Alban LESCOUT <elie-alban.lescout@ensg.eu>
2022-01-12 22:17:22 +0100

    more explicit variable name

--- potree_publisher.py
+++ potree_publisher.py
@@ -18,15 +18,15 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    input_point_cloud: str = typer.Argument(..., help="Path to the point cloud to process. Any type supported by PotreeConverter is possible."),
+    input_path: str = typer.Argument(..., help="Path to the point cloud or a folder of point clouds to process. Any type supported by PotreeConverter is possible."),
     potree_server_root: str = typer.Option(cfg['root'], help='Root path of the potree server.', callback=check_path_callback),
     point_cloud_folder: str = typer.Option(cfg['point_cloud_folder'], help='Folder where the point cloud will be stored after conversion to Potree Format.'),
     viewer_folder: str = typer.Option(cfg['viewer_folder'], help='Folder where the viewer html page will be stored.')
     ):  
     
-    input_point_cloud = Path(input_point_cloud)
+    input_path = Path(input_path)
 
-    typer.echo(f"Let's publish {input_point_cloud}!")
+    typer.echo(f"Let's publish {input_path}!")
 
     publish = Publisher(
         potree_server_root,
@@ -34,10 +34,10 @@ def main(
         viewer_folder
     )
 
-    if input_point_cloud.is_file():
-        publish.single_file(input_point_cloud)
-    else:
-        raise NotImplementedError("Folder case coming soon.")
+    if input_path.is_file():
+        publish.single_file(input_path)
+    elif input_path.is_dir():
+        publish.folder(input_path)
 
 
 if __name__ == "__main__":
